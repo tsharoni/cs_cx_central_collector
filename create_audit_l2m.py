d3f79870586a95ec1f1f8f62bb7ea458c7cec443
isaac <tzachi.sharoni@coralogix.com>
2024-08-30 18:13:38 -0400

    E2M to automatically create views-usage metrics, + readme updates

--- create_audit_l2m.py
+++ create_audit_l2m.py
@@ -27,11 +27,24 @@ if __name__ == '__main__':
         create_e2m(
             region=cx_api_key_region,
             key=cx_api_key,
+            name="views_usage",
+            description="",
+            query="_exists_:action_details.operation.operation_payload.queryDef.selectedViewId\\tAND\\tNOT\\taction_details.operation.operation_payload.queryDef.selectedViewId:\\\"\\\-1\\\"",
+            labels_list={"view_name": "action_details.operation.operation_payload.dashboardId_enriched",
+                         "username": "actor.username",
+                         "team_name": "action.team_name",
+                         "action": "action_details.operation.action"},
+            permutation=30000
+        )
+
+        create_e2m(
+            region=cx_api_key_region,
+            key=cx_api_key,
             name="Users",
             description="",
             query="_exists_:actor.username\\tAND\\tNOT\\tcoralogix.metadata.applicationName:/.*_audit/",
             labels_list={
-                "dashboard_name": "action_details.operation.operation_payload.dashboardId_enriched",
+                "dashboard_name": "action_details.operation.operation_payload.queryDef.selectedViewId_enriched",
                 "username": "actor.username",
                 "coralogix_team": "action.team_name",
                 "action": "action_details.operation.action",

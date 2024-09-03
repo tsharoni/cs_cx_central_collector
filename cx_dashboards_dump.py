c0ca5163246dca24cc4c790c7a67ced65c18fb18
isaac <tzachi.sharoni@coralogix.com>
2024-09-03 16:36:44 -0400

    Adding support to dump grafanas+alerts

--- cx_dashboards_dump.py
+++ cx_dashboards_dump.py
@@ -16,8 +16,39 @@ def dump_dashboards(region, key, file_prefix):
         if not dashboard_file:
             continue
 
-        output = open("{}-{}-{}.json".format(file_prefix,dashboards[dashboard_id], dashboard_id), 'w')
+        output = open("{}-{}-{}.json".format(file_prefix, dashboards[dashboard_id], dashboard_id), 'w')
         output.write(dashboard_file)
+        output.close()
+
+
+def dump_grafana_dashboards(region, key, file_prefix):
+
+    dashboards = cx_infra.get_grafana_dashboards(region, key)
+    if not dashboards:
+        print('Failed to retrieve grafana dashboards')
+        return
+
+    for dashboard in dashboards:
+        dashboard_file = cx_infra.get_grafana_dashboard_file(region, key, dashboard['uid'])
+
+        if not dashboard_file:
+            continue
+
+        output = open("{}-{}-{}.json".format(file_prefix, dashboard['uid'], dashboard['title']), 'w')
+        output.write(dashboard_file)
+        output.close()
+
+
+def dump_alerts(region, key, file_prefix):
+
+    alerts = cx_infra.call_http_extended(cx_infra.coralogix_alerts_url.format(cx_infra.region_domains[region]), key)
+    if not alerts:
+        print('Failed to retrieve alerts')
+        return
+
+    output = open("{}-alerts.json".format(file_prefix), 'w')
+    output.write(alerts)
+    output.close()
 
 
 if __name__ == '__main__':
@@ -39,5 +70,7 @@ if __name__ == '__main__':
         for team in account["teams"]:
             print("team: {}".format(team["name"]))
             # special setup to get the list of users
-            dump_dashboards(region=team["region"], key=team["key"], file_prefix="cx-{}".format(team["name"]))
+            dump_dashboards(region=team["region"], key=team["key"], file_prefix="cx_dashboards-{}".format(team["name"]))
+            dump_grafana_dashboards(region=team["region"], key=team["key"], file_prefix="grafana_dashboards-{}".format(team["name"]))
+            dump_alerts(region=team["region"], key=team["key"], file_prefix="alerts-{}".format(team["name"]))
 

971d320781601114eee8382131cff565ba2691eb
isaac <tzachi.sharoni@coralogix.com>
2024-11-12 17:10:50 -0500

    Adding comments to new script

--- custom_dashboard_deployment.py
+++ custom_dashboard_deployment.py
@@ -5,6 +5,22 @@ from os import environ
 from cx_infra import create_dashboard, delete_dashboard, replace_dashboard, get_dashboards
 
 
+##########################################################################################
+# custom_dashboard_deployment recieves:
+#    dasoboard file as a mako file template
+#    keypairs value to replace placeholders in template with values
+#
+# to launch the script use:
+#    python3 custom_dashboard_deployment dashboard.mako --params key1=value1 key2=value2
+#
+# the template should contain the keys/placeholder in ${<key>} format like:
+#    ..."name": "[Coralogix] ${team_name} Data Usage and Cost estimate"...
+#
+# In the mako file , the ${team_name}, if added to template, would be replaced with the
+#    team_name from the teams.json
+##########################################################################################
+
+
 def parse_unknown_args(args):
     """Parse unknown arguments in key=value format into a dictionary."""
     parsed_args = {}
@@ -18,10 +34,12 @@ def parse_unknown_args(args):
 
 
 def retrieve_dashboard_id(dashboard_name, region, key):
+    # retrieves the ID from the name of the dashboard
     dashboards = get_dashboards(region, key)
     for dashboard in dashboards:
         if dashboards[dashboard] == dashboard_name:
             return dashboard
+
 
 if __name__ == '__main__':
 
@@ -55,14 +73,18 @@ if __name__ == '__main__':
         for team in account['teams']:
             print("team: {}".format(team['name']))
 
+            # rendering the mako file from the parameters provided
             params_dict['team_name'] = team['name']
             rendered_file = Template(file_content).render(**params_dict)
             dashboard_json = json.loads(rendered_file)
 
+            # removing updatedAt as it is not accepted by the service
             del dashboard_json['updatedAt']
+            # check if the dashboard already exists by retrieving the dashboard_id
             dashboard_id = retrieve_dashboard_id(dashboard_json['dashboard']['name'], team['region'], team['key'])
 
             if dashboard_id:
+                # replace if exists
                 dashboard_json['dashboard']['id'] = dashboard_id
                 replace_dashboard(
                     region=team['region'],
@@ -70,6 +92,7 @@ if __name__ == '__main__':
                     dashboard_data=json.dumps(dashboard_json)
                 )
             else:
+                # create if doesn't (remove the ID to create a new one)
                 del dashboard_json['dashboard']['id']
                 create_dashboard(
                     region=team['region'],
